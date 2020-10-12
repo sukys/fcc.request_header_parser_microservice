856b9b8be2d748df3ab8df398173a7ebc9ee1927
Luís Sukys <sukys22@gmail.com>
2020-10-12 19:00:02 -0300

    desafio finalizado

--- README.md
+++ README.md
@@ -8,3 +8,7 @@
 
 #### Example output:
 * `{"ipaddress":"159.20.14.100","language":"en-US,en;q=0.5","software":"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:50.0) Gecko/20100101 Firefox/50.0"}`
+
+
+#### App working at:
+https://boilerplate-project-headerparser.sukys22.repl.co
--- server.js
+++ server.js
@@ -25,6 +25,11 @@ app.get("/api/hello", function (req, res) {
 });
 
 
+app.get("/api/whoami", function (req, res) {
+    console.log(req.headers)
+    res.json({"ipaddress": req.headers['x-forwarded-for'], "language": req.headers['accept-language'], "software": req.headers['user-agent']})
+});
+
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
